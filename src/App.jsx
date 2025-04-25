1ea9e5b4459d0e69c4fb04fef118cb563fe3942f
gabriel silva <gabriel.spaulo@eaportal.org>
2025-04-25 12:35:26 -0300

    blog

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,19 @@ function App() {
 
   return (
     <>
-      
+      <h1 className="title"> Biografia </h1>
+      <div className="container">
+        <div className="section">
+          <img className="img" src="https://img.freepik.com/fotos-gratis/plano-de-fundo-texturizado-de-concreto-grunge-preto_53876-124541.jpg?semt=ais_hybrid&w=740" alt="" />
+          <h4 className="subtitle">Gabriel S.</h4>
+        </div>
+        <p text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris auctor blandit nibh non suscipit. Donec at mi at arcu molestie laoreet. Donec posuere nisi at augue mollis iaculis. Donec volutpat, mi eu faucibus efficitur, metus velit viverra orci, vel sagittis arcu eros quis mi. Aenean viverra, quam ut suscipit gravida, eros turpis aliquet risus, a mollis dui ante in ipsum. Donec nunc nisl, auctor in mollis dapibus, volutpat ac massa. Nullam nec arcu eu eros pellentesque scelerisque. In et faucibus enim. Nam a nisl felis. Integer et est diam.</p>
+        <p>25/04/2025 às 12:10</p>
+        <div>
+
+        </div>
+
+      </div>
     </>
   )
 }
